78c5c7cf775c2ed33492108ff82ae5a38ca3cdc4
Кирилл Дьяков (ноутбук) <Dyakovks@gmail.com>
2019-02-23 12:32:07 +0300

    Добавлен Dagger. Создан AppComponent

---
+++ app/src/main/java/ru/today/news/NewsApp.kt
@@ -0,0 +1,29 @@
+package ru.today.news
+
+import android.app.Application
+import android.content.res.Resources
+import ru.today.news.injection.AppComponent
+import timber.log.Timber
+
+class NewsApp : Application() {
+
+    companion object {
+
+        lateinit var instance: NewsApp
+            private set
+
+        lateinit var appComponent: AppComponent
+            private set
+
+        val res: Resources
+            get() = instance.resources
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+
+        instance = this
+    }
+}
