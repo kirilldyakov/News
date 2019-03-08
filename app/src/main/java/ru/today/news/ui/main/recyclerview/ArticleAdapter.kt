e88797505178da59800c08517bf2296d3f5dfd0e
Кирилл Дьяков (ноутбук) <Dyakovks@gmail.com>
2019-03-08 08:21:04 +0300

    Добавлен diffcallback

--- app/src/main/java/ru/today/news/ui/main/recyclerview/ArticleAdapter.kt
+++ app/src/main/java/ru/today/news/ui/main/recyclerview/ArticleAdapter.kt
@@ -1,34 +1,59 @@
 package ru.today.news.ui.main.recyclerview
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
+import androidx.annotation.NonNull
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
 import ru.today.news.R
 import ru.today.news.data.remote.responses.everything.ArticlesItem
+import ru.today.news.databinding.CardArticleBinding
 import ru.today.news.injection.scopes.PerFragment
 import ru.today.news.util.Utils
-import timber.log.Timber
-import javax.inject.Inject
 
 //https://startandroid.ru/ru/courses/architecture-components/27-course/architecture-components/542-urok-14-paging-library-chast-1.html
+//https://medium.com/@paulhundal/paging-library-with-android-mvvm-6a3a4d94de72
 @PerFragment
-class ArticleAdapter: PagedListAdapter<ArticlesItem, ArticleViewHolder>() {
+class ArticleAdapter: PagedListAdapter<ArticlesItem, ArticleViewHolder>(diff_callback) {
 
+    companion object {
+        val diff_callback: DiffUtil.ItemCallback<ArticlesItem> = object : DiffUtil.ItemCallback<ArticlesItem>() {
+            override fun areItemsTheSame(@NonNull oldItem: ArticlesItem, @NonNull newItem: ArticlesItem): Boolean {
+                return oldItem.url === newItem.url
+            }
 
+            override fun areContentsTheSame(@NonNull oldItem: ArticlesItem, @NonNull newItem: ArticlesItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Utils.createViewHolder(parent, R.layout.card_article, ::ArticleViewHolder)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val viewBinding : CardArticleBinding = CardArticleBinding.inflate(layoutInflater, parent, false)
+        return ViewHolder(viewBinding)
     }
 
     override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//        holder.viewModel.update(getItem(position))
+//        holder.executePendingBindings()
+        val viewHolder = holder as ViewHolder
+        getItem(position)?.let {
+            viewHolder.bind(it)
+        }
+    }
+
+    class ViewHolder(private val binding : CardArticleBinding) : RecyclerView.ViewHolder(       binding.root) {
+        fun bind(model : ArticlesItem) {
+            binding.tvArticleTitle = model.title
+        }
     }
 }
+}
+
+
 //@Inject
 //constructor() : RecyclerView.Adapter<ArticleViewHolder>(), FastScrollRecyclerView.SectionedAdapter {
 //
