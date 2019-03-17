9392a8228e36e8072b3a52f7ade3f77062389eda
Kirill Dyakov <Dyakovks@gmail.com>
2019-03-17 15:21:43 +0300

    Добавлен NavigationDrawer

--- app/src/main/java/ru/today/news/ui/main/MainActivity.kt
+++ app/src/main/java/ru/today/news/ui/main/MainActivity.kt
@@ -1,6 +1,11 @@
 package ru.today.news.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import ru.today.news.R
 import ru.today.news.data.db.NewsDatabase
 import ru.today.news.data.db.category.Category
@@ -12,7 +17,11 @@ import ru.today.news.ui.main.viewpager.MainAdapter
 import javax.inject.Inject
 
 
-class MainActivity : BaseActivity<ActivityMainBinding, NoOpViewModel<MvvmView>>(), MvvmView {
+class MainActivity : BaseActivity<ActivityMainBinding, NoOpViewModel<MvvmView>>(), MvvmView,
+    NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     @Inject
     lateinit var adapter: MainAdapter
@@ -35,14 +44,23 @@ class MainActivity : BaseActivity<ActivityMainBinding, NoOpViewModel<MvvmView>>(
         binding.viewPager.adapter = adapter
         binding.tabLayout.setupWithViewPager(binding.viewPager)
 
+        fillupCategories()
+
+        var toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,R.string.nav_open, R.string.nav_close  )
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun fillupCategories() {
         newsDatabase.categoryDao.deleteAll()
-        newsDatabase.categoryDao.insert(Category("business", "бизнес",0))
-        newsDatabase.categoryDao.insert(Category("entertainment", "здоровье",0))
-        newsDatabase.categoryDao.insert(Category("general", "главные",0))
-        newsDatabase.categoryDao.insert(Category("health", "здоровье",0))
-        newsDatabase.categoryDao.insert(Category("science", "наука",0))
-        newsDatabase.categoryDao.insert(Category("sports", "спорт",0))
-        newsDatabase.categoryDao.insert(Category("technology", "технологии",0))
+        newsDatabase.categoryDao.insert(Category("business", "бизнес", 0))
+        newsDatabase.categoryDao.insert(Category("entertainment", "здоровье", 0))
+        newsDatabase.categoryDao.insert(Category("general", "главные", 0))
+        newsDatabase.categoryDao.insert(Category("health", "здоровье", 0))
+        newsDatabase.categoryDao.insert(Category("science", "наука", 0))
+        newsDatabase.categoryDao.insert(Category("sports", "спорт", 0))
+        newsDatabase.categoryDao.insert(Category("technology", "технологии", 0))
     }
 
 
